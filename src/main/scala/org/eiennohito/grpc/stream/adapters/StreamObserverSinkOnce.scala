28b81e16ba781ed3f535dadd792f78ee0e766dcc
Arseny Tolmachev <eiennohito@gmail.com>
2016-07-04 19:29:47 +0900

    add command line launchers for dpp sentence extraction

--- src/main/scala/org/eiennohito/grpc/stream/adapters/StreamObserverSinkOnce.scala
+++ src/main/scala/org/eiennohito/grpc/stream/adapters/StreamObserverSinkOnce.scala
@@ -2,13 +2,15 @@ package org.eiennohito.grpc.stream.adapters
 
 import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler}
 import akka.stream.{Attributes, Inlet, SinkShape}
+import com.typesafe.scalalogging.StrictLogging
+import io.grpc.Status
 import io.grpc.stub.StreamObserver
 
 /**
   * @author eiennohito
   * @since 2016/04/28
   */
-class StreamObserverSinkOnce[T](so: StreamObserver[T]) extends GraphStage[SinkShape[T]] {
+class StreamObserverSinkOnce[T](so: StreamObserver[T]) extends GraphStage[SinkShape[T]] with StrictLogging {
   val in: Inlet[T] = Inlet("StreamObserverOnce")
   override val shape = SinkShape(in)
 
@@ -27,7 +29,8 @@ class StreamObserverSinkOnce[T](so: StreamObserver[T]) extends GraphStage[SinkSh
         }
 
         override def onUpstreamFailure(ex: Throwable) = {
-          so.onError(ex)
+          logger.error("error in stream observer once", ex)
+          so.onError(Status.INTERNAL.withCause(ex).asException())
         }
       })
 
